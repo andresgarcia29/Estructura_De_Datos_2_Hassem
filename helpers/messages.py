e4f2961f44a99df629f200b725951c2e40f4ef71
Andrés García <joseandres@digitt.com>
2018-08-22 10:27:06 -0500

    Make a function to create messages

--- helpers/messages.py
+++ helpers/messages.py
@@ -8,3 +8,21 @@ messages = {
         'contract': 'The contract is perfectly'
     }
 }
+
+def make_message_to_send(name, action, status):
+    verb = ''
+    if status == 'success':
+        status = 'correctly'
+    else:
+        status = 'with error in the process'
+    if action == 'create':
+        verb = 'created'
+    elif action == 'update':
+        verb = 'updated'
+    elif action == 'delete':
+        verb = 'deleted'
+    else:
+        verb = 'Has no verb'
+    return {
+        'message': str(name).title() + verb
+    }
